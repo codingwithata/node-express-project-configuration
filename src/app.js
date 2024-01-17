9ac207d3f93d6abf8e2ecc7ac2c3a89993562dcc
Malik <Mata@indeed.com>
2024-01-17 14:00:49 -0800

    Add routes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,30 @@ const morgan = require("morgan");
 
 const app = express();
 
-const sayHello = (req, res, next) => {
-  res.send("Hello");
+const sayHello = (req, res) => {
+  console.log(req.query);
+  const name = req.query.name;
+  const content = name ? `Hello, ${name}!` : "Hello!";
+  res.send(content);
+};
+
+const saySomething = (req, res) => {
+  const greeting = req.params.greeting;
+  const name = req.query.name;
+
+  const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+  res.send(content);
 };
 
 app.use(morgan("dev"));
 
 app.get("/hello", sayHello);
 
-app.get("/hello/:name", (req, res) => {
-  res.send(`Hello ${req.params.name}`);
+app.get("/say/:greeting", saySomething);
+
+app.get("/songs", (req, res) => {
+  const title = req.query.title;
+  res.send(title);
 });
 
 module.exports = app;
