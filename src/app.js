97413f6ab9dc2f6bbbd91e2c715521467dc91c5a
Malik <Mata@indeed.com>
2024-01-17 08:47:13 -0800

    Add morgan for logging

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
+const sayHello = (req, res, next) => {
+  res.send("Hello");
+};
+
+app.use(morgan("dev"));
+
+app.use("/", sayHello);
 
 module.exports = app;
